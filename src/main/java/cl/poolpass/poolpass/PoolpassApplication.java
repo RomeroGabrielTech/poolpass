5d056762b169d7ebf8d475824f8fb37c79f66307
Gabriel Romero <gaen.roca@gmail.com>
2023-02-10 11:47:32 -0300

    Commit inicial. Se levanta la app directamente como viene de Spring Inizialaizer

---
+++ src/main/java/cl/poolpass/poolpass/PoolpassApplication.java
@@ -0,0 +1,13 @@
+package cl.poolpass.poolpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolpassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoolpassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/poolpass/poolpass/PoolpassApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.poolpass.poolpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoolpassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
